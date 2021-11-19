757118a1da8f261b0a9fdea537442c968bc67118
Nuri Engin <nuri@engin.web.tr>
2021-11-19 19:13:59 +0300

    ✨ : Database connection introduced to the MVC app

---
+++ 14_2_mvc_app/Database.php
@@ -0,0 +1,31 @@
+<?php
+
+    namespace app;
+
+    use PDO;
+
+    class Database 
+    {
+        public \PDO $pdo;
+        
+        public function __construct()
+        {
+            $this->pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+
+        public function getProducts($search = '')
+        {
+            if (!empty($search)) {
+                $statment = $this->pdo->prepare('SELECT * FROM products WHERE title LIKE :title ORDER BY create_date DESC');
+                $statment->bindValue(':title', "%$search%");
+            } else {
+                //exec is not best practise
+                $statment = $this->pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+            }
+
+            $statment->execute();
+            return $statment->fetchAll(PDO::FETCH_ASSOC); // Fetch as Associative array.
+        }
+    }
+?>

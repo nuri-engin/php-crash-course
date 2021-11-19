0ddae30638e01fc875e2813c3491ddf0ad1a4b04
Nuri Engin <nuri@engin.web.tr>
2021-11-19 19:03:54 +0300

    ✨ : A basic MVS structure with the routes

---
+++ 14_2_mvc_app/controllers/ProductController.php
@@ -0,0 +1,26 @@
+<?php
+    namespace app\controllers;
+
+    class ProductController
+    {
+        public function index()
+        {
+            echo "Index page";
+        }
+
+        public function create()
+        {
+            echo "Create page";            
+        }
+
+        public function update()
+        {
+            echo "Update page";            
+        }
+
+        public function delete()
+        {
+            echo "Delete page";            
+        }
+    }
+?>
